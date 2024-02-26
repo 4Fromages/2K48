58cd82198b31168fe05459a9d1152419dd0a4336
tomleblais <leblaistom@gmail.com>
2024-02-26 11:41:18 +0100

    fix: merge direction fixed

--- resources/scripts/lib/moves/MoveLeftStrategy.mjs
+++ resources/scripts/lib/moves/MoveLeftStrategy.mjs
@@ -1,9 +1,33 @@
 import { MoveStrategy } from "./MoveStrategy.mjs"
 
 export class MoveLeftStrategy extends MoveStrategy {
+    #grid    
 
     constructor(grid) {
         super(grid)
+        this.#grid = grid
+    }
+
+    getFirstMoveableTile() {
+        this.#grid.tiles.sort((tileA, tileB) => {
+            if (tileA.getY() > tileB.getY()) return -1
+            if (tileA.getY() < tileB.getY()) return 1
+            if (tileA.getX() > tileB.getX()) return -1
+            if (tileA.getX() < tileB.getX()) return 1
+            return 0
+        })
+        return super.getFirstMoveableTile()
+    }
+
+    getFirstMergeableTile() {
+        this.#grid.tiles.sort((tileA, tileB) => {
+            if (tileA.getY() > tileB.getY()) return 1
+            if (tileA.getY() < tileB.getY()) return -1
+            if (tileA.getX() > tileB.getX()) return 1
+            if (tileA.getX() < tileB.getX()) return -1
+            return 0
+        })
+        return super.getFirstMergeableTile()
     }
     
     getNextCoords(tile) {
--- resources/scripts/lib/moves/MoveRightStrategy.mjs
+++ resources/scripts/lib/moves/MoveRightStrategy.mjs
@@ -1,15 +1,39 @@
 import { MoveStrategy } from "./MoveStrategy.mjs"
 
 export class MoveRightStrategy extends MoveStrategy {
+    #grid
 
     constructor(grid) {
         super(grid)
+        this.#grid = grid
     }
-    
+
+    getFirstMoveableTile() {
+        this.#grid.tiles.sort((tileA, tileB) => {
+            if (tileA.getY() > tileB.getY()) return 1
+            if (tileA.getY() < tileB.getY()) return -1
+            if (tileA.getX() > tileB.getX()) return 1
+            if (tileA.getX() < tileB.getX()) return -1
+            return 0
+        })
+        return super.getFirstMoveableTile()
+    }
+
+    getFirstMergeableTile() {
+        this.#grid.tiles.sort((tileA, tileB) => {
+            if (tileA.getY() > tileB.getY()) return -1
+            if (tileA.getY() < tileB.getY()) return 1
+            if (tileA.getX() > tileB.getX()) return -1
+            if (tileA.getX() < tileB.getX()) return 1
+            return 0
+        })
+        return super.getFirstMergeableTile()
+    }
+
     getNextCoords(tile) {
         return [tile.getX() + 1, tile.getY()]
     }
-    
+
     getPreviousCoords(tile) {
         return [tile.getX() - 1, tile.getY()]
     }
